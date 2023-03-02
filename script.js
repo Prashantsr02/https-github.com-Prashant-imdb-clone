95f543188e0f207ec7cb20d326a7e6bbc8603480
Akash Padampalle <45806342+akashpadampalle@users.noreply.github.com>
2023-03-02 14:16:59 +0530

    Add files via upload
    3 files added to the project
         1. index.hml
         2. script.js
         3. style.css

---
+++ script.js
@@ -0,0 +1,15 @@
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '514924d08fmshcef634114389e8cp1fb309jsnbee220139160',
+// 		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=most%20popular%20movies', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+
+console.log("script is working !!!");
